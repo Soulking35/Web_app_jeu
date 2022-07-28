ab069823a9c5e10877e203a1de20aa76f4967dd8
Lino <>
2022-07-28 09:34:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request  
+from modules import des
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/jeux', methods=['GET', 'POST'])
+def fonction_jeu():
+    mon_tirage = des.tirage_des().tirage()
+    choix_joueur = request.form.get('le_pari')
+    resultat = des.tirage_des().verification(choix_joueur,mon_tirage)
+    print(choix_joueur)
+    return render_template('page_jeux.html',resu = resultat, tira = mon_tirage)
+
+#app.run(host='0.0.0.0', port=81)
---
+++ modules/des.py
@@ -0,0 +1,13 @@
+import random
+
+class tirage_des:
+  def tirage(self):
+    le_tirage = random.randint(0,6)
+    return le_tirage
+
+  def verification(self,choix_joueur,tirage):
+    if choix_joueur == str(tirage):
+      resultat='gagner'
+    else:
+      resultat='perdu'
+    return resultat 
